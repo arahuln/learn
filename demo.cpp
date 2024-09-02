30f90214edf11e6b64fee5157ba2e8852eb979e3
ANGOTHU RAHUL NAIK <21je0110@iitism.ac.in>
2024-09-03 01:02:08 +0530

    testing added demo.cpp file

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        cout<<"test";
+    }
+}
